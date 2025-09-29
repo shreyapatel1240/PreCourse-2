51babfe06e07a44d1f41a42f8fa620fbe26fcc72
Shreya Patel <shreyapatel1240@gmail.com>
2025-09-28 21:32:30 -0400

    Found mid point of a singly linked list

--- Exercise_3.js
+++ Exercise_3.js
@@ -1,31 +1,45 @@
-class LinkedList {
+// Time Complexity : O(n)
+// Space Complexity : O(1)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : No
+class SinglyLinkedList {
     constructor() {
-        this.head = null; // head of linked list
+        this.head = null // head of linked list
     }
-​
+
     /* Linked list node */
     static Node = class {
-       constructor(d) {
-       //Constructor here
-       this.data = d;
-       this.next = null;
-       }
+        constructor(d) {
+            //Constructor here
+            this.data = d
+            this.next = null
+        }
     }
-​
+
     /* Function to print middle of linked list */
     //Complete this function
-    function printMiddle() {
+    printMiddle() {
         //Write your code here
-       	//Implement using Fast and slow pointers
+        //Implement using Fast and slow pointers
+        if (!this.head) return null
+
+        let last = this.head
+        let first = this.head
+
+        while (first && first.next) {
+            last = last.next
+            first = first.next.next
+        }
+        console.log("Middle Node: " + last.data)
     }
-​
-    function push(new_data) {
-        let new_node = new this.Node(new_data);
+
+    push(new_data) {
+        let new_node = new SinglyLinkedList.Node(new_data);
         new_node.next = this.head;
         this.head = new_node;
     }
-​
-    function printList() {
+
+    printList() {
         let tnode = this.head;
         while (tnode != null) {
             console.log(tnode.data + "->");
@@ -34,8 +48,8 @@ class LinkedList {
         console.log("NULL");
     }
 }
-​
-let llist = new LinkedList();
+
+let llist = new SinglyLinkedList();
 for (let i = 15; i > 0; --i) {
     llist.push(i);
     llist.printList();
